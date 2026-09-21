6a2fde6d4374264ae9574e12c158e1f5ae6986fa
Anirudh-786 <eventscreative142@gmail.com>
2026-09-21 06:03:15 +0530

    main method added for code excution

--- src/main/java/org/example/EvenNumberOfDigits.java
+++ src/main/java/org/example/EvenNumberOfDigits.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class EvenNumberOfDigits {
     public int findNumbers(int[] nums) {
         int evenCount = 0;
@@ -11,5 +13,14 @@ public class EvenNumberOfDigits {
         }
 
         return evenCount;
+    }
+
+    static void main() {
+        EvenNumberOfDigits evenNumber = new EvenNumberOfDigits();
+        int [] nums = {-4,-1,0, 3, 10};
+        System.out.println("Original Array: " + Arrays.toString(nums));
+
+        int count = evenNumber.findNumbers(nums);
+        System.out.println("Even number count : " + count);
     }
 }
